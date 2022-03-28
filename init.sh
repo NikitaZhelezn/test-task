d3ee227fe51e0fb68dee83d7f11c2581c2b59e80
NikitaZhelezn <nikzhezp@gmail.com>
2022-03-28 09:42:53 +0300

    change init.sh

--- init.sh
+++ init.sh
@@ -2,8 +2,8 @@
 
 cp .env.example .env
 cd laradock
-docker-compose exec workspace composer install
 docker-compose exec workspace chmod 777 -R /var/www
+docker-compose exec workspace composer install
 docker-compose exec workspace php artisan key:generate
 docker-compose exec workspace php artisan migrate
 docker-compose exec workspace php artisan db:seed
